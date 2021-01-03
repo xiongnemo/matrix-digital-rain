fd27a1f33c589f94a8e604ba618cfdcec068d279
Nemo Xiong <xiongnemo@126.com>
2021-01-04 06:45:21 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # matrix-digital-rain
 Marshmallow's implementation of Matrix Digital Rain (the movie)
+
+## Usage
+
+Open up `demo.html`
+
+## Feature
+
+* full redraw when window is resized
+* no scroll bar
+
+## Screenshot
+
+![](./img/demo.png)
---
+++ js/matrix.js
@@ -0,0 +1,52 @@
+function draw_matrix() {
+
+    // get canvas
+    var c = document.getElementById("c");
+    var ctx = c.getContext("2d");
+
+    // make the canvas full screen
+    c.height = window.outerHeight;
+    c.width = window.outerWidth;
+
+    // charset to use
+    var matrix_charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789あいうえおかきくけこさしすせそたちつてとはひふへほなにぬねのまみむめもらりるれろやゆよんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽアイウエオカキクケコサシスセソタチツテトハヒフヘホナニヌネノマミムメモラリルレロヤユヨンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポ";
+    // convert the string into an array of single characters
+    var matrix = matrix_charset.split("");
+
+
+    var font_size = 20;
+    var columns = c.width / font_size; //number of columns for the rain
+    //an array of drops - one per column
+    var drops = [];
+    //x below is the x coordinate
+    //1 = y co-ordinate of the drop(same for every drop initially)
+    for (var x = 0; x < columns; x++)
+        drops[x] = 1;
+
+    // draw the characters
+    function draw() {
+        // Black BG for the canvas
+        // translucent BG to show trail
+        ctx.fillStyle = "rgba(0, 0, 0, 0.08)";
+        ctx.fillRect(0, 0, c.width, c.height);
+
+        ctx.fillStyle = "#26c749"; // green text
+        ctx.font = font_size + "px arial";
+        // looping over drops
+        for (var i = 0; i < drops.length; i++) {
+            // a random chinese character to print
+            var text = matrix[Math.floor(Math.random() * matrix.length)];
+            // x = i * font_size, y = value of drops[i]*font_size
+            ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+            // sending the drop back to the top randomly after it has crossed the screen
+            // adding a randomness to the reset to make the drops scattered on the Y axis
+            if (drops[i] * font_size > c.height && Math.random() > 0.975)
+                drops[i] = 0;
+
+            // incrementing Y coordinate
+            drops[i]++;
+        }
+    }
+    setInterval(draw, 60);
+}
